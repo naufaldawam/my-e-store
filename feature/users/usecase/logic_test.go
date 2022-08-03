8d82580bd085969eb415c238ce8f64cb3cff26d3
ivands26 <ivandwis.fis12@gmail.com>
2022-08-03 08:17:43 +0700

    unit test 86%

--- feature/users/usecase/logic_test.go
+++ feature/users/usecase/logic_test.go
@@ -53,3 +53,70 @@ func TestAddUser(t *testing.T) {
 	})
 
 }
+
+func TestDeleteCase(t *testing.T) {
+	repo := new(mocks.UserData)
+
+	t.Run("Success Delete", func(t *testing.T) {
+		repo.On("DeleteData", mock.Anything).Return(1, nil)
+
+		srv := New(repo, validator.New())
+		delete, err := srv.DeleteCase(1)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 1, delete)
+	})
+}
+
+func TestUpdateCase(t *testing.T) {
+	insertData := domain.User{
+		Name:     "ivan",
+		Email:    "ivan@gmail.com",
+		Password: "123",
+		Phone:    "081217076500",
+	}
+
+	repo := new(mocks.UserData)
+
+	t.Run("Success Update", func(t *testing.T) {
+		repo.On("UpdateData", mock.Anything, 1).Return(1, nil).Once()
+
+		srv := New(repo, validator.New())
+		update, err := srv.UpdateCase(insertData, 1)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, update)
+	})
+
+}
+
+func TestGetProfile(t *testing.T) {
+	insertData := domain.User{
+		Name:     "ivan",
+		Email:    "ivan@gmail.com",
+		Password: "123",
+		Phone:    "081217076500",
+		Role:     "user",
+	}
+
+	repo := new(mocks.UserData)
+
+	t.Run("Data Found", func(t *testing.T) {
+		repo.On("GetSpecific", 1).Return(insertData, nil).Once()
+
+		srv := New(repo, validator.New())
+		myprofile, err := srv.GetProfile(1)
+		assert.NoError(t, err)
+		assert.Equal(t, insertData, myprofile)
+
+	})
+
+	t.Run("Data Not Found", func(t *testing.T) {
+		repo.On("GetSpecific", 0).Return(insertData, errors.New("error")).Once()
+
+		srv := New(repo, validator.New())
+		notfound, err := srv.GetProfile(0)
+
+		assert.NotNil(t, err)
+		assert.NotEqual(t, insertData, notfound)
+	})
+}
